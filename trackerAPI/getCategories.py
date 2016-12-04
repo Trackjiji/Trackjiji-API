1cc5e523d762fb72147b628026879229b0139973
seanrt <seanrtohidi@gmail.com>
2016-12-03 21:08:16 -0500

    The categories don't get scraped by heroku; need to load JS from the page somehow

--- trackerAPI/getCategories.py
+++ trackerAPI/getCategories.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python3
 
-from urllib.request import urlopen
+# from urllib.request import urlopen
+import dryscrape
 from bs4 import BeautifulSoup
-import json
 import copy
 
 # This function grabs all the categories to be used in the Kijiji links
 def getCategories():
     categories = []
-    url = 'http://www.kijiji.ca/'
-    html = urlopen(url).read()
+    url = 'http://kijiji.ca/'
+    # html = urlopen(url).read()
+    session = dryscrape.Session()
+    session.visit(url)
+    html = session.body()
     soup = BeautifulSoup(html, 'html.parser')
+    # return soup.text
     links = soup.find(id='Categories').find('ul').find_all('a')
     for thing in links:
         thing = thing.get('href')
